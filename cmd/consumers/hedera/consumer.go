0c9f66518896a8f3ce4a243081595de997647048
Tarek Zaarour <tareqzaarour@gmail.com>
2024-07-31 18:45:26 +0100

    using new publisher package

--- cmd/consumers/hedera/consumer.go
+++ cmd/consumers/hedera/consumer.go
@@ -4,38 +4,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
-	sub "kubeinsights/pkg/hedera"
+	"github.com/tarzaa1/kubeinsights/pkg/publisher"
 
 	"github.com/hashgraph/hedera-sdk-go/v2"
 )
 
 func main() {
 
-	client, err := sub.ClientFromFile("sub_config.json")
+	config := publisher.ReadHederaConfig("sub_config.json")
+	client := publisher.HederaClient(config)
 
-	if err != nil {
-		fmt.Printf("Failed to create hedera client: %s\n", err)
-		os.Exit(1)
-	}
-
-	// Get the topic ID from the transaction receipt
 	topicID, err := hedera.TopicIDFromString("0.0.1003")
 
 	if err != nil {
-		println(err.Error(), ": error creating topic")
-		return
+		log.Fatalf(err.Error(), ": error creating topic")
 	}
 
-	//Log the topic ID to the console
 	fmt.Printf("topicID: %v\n", topicID)
 
 	// Subscribe to the topic
 	_, err = hedera.NewTopicMessageQuery().
 		SetTopicID(topicID).
-		Subscribe(client.Client, func(message hedera.TopicMessage) {
+		Subscribe(client, func(message hedera.TopicMessage) {
 			var record map[string]interface{}
 			err := json.Unmarshal(message.Contents, &record)
 			if err != nil {
