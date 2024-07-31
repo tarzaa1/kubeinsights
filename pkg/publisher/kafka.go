5b13b3301a6636a196f5b15dda1de53636b148b8
Tarek Zaarour <tareqzaarour@gmail.com>
2024-07-31 18:16:21 +0100

    NewKafkaPublisher implementation and small fixes

--- pkg/publisher/kafka.go
+++ pkg/publisher/kafka.go
@@ -10,17 +10,51 @@ import (
 )
 
 type KafkaPublisher struct {
-	AdminClient *kafka.AdminClient
-	Client      *kafka.Producer
-	Topics      map[string]bool
+	Client  *kafka.Producer
+	TopicID string
 }
 
-func NewKafkaPublisher(address string) *KafkaPublisher {
+func (p KafkaPublisher) SubmitMessage(message []byte) string {
+
+	delivery_chan := make(chan kafka.Event, 10000)
+	err := p.Client.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &p.TopicID, Partition: kafka.PartitionAny},
+		Value:          message},
+		delivery_chan,
+	)
+
+	if err != nil {
+		close(delivery_chan)
+		return fmt.Sprintf("Produce failed: %s\n", err)
+	}
+
+	e := <-delivery_chan
+	m := e.(*kafka.Message)
+
+	var status string
+
+	if m.TopicPartition.Error != nil {
+		status = fmt.Sprintf("Delivery failed: %v\n", m.TopicPartition.Error)
+	} else {
+		status = fmt.Sprintf("Delivered message to topic %s [%d] at offset %v\n",
+			*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+	}
+	close(delivery_chan)
+	return status
+}
+
+func NewKafkaPublisher(address string, topicID string) *KafkaPublisher {
 
 	adminClient, err := kafka.NewAdminClient(&kafka.ConfigMap{"bootstrap.servers": address})
 	if err != nil {
 		log.Fatalf("Failed to create Admin client: %s", err)
 	}
+
+	if err := NewKafkaTopic(adminClient, topicID); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	adminClient.Close()
 
 	producer, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": address})
@@ -29,44 +63,10 @@ func NewKafkaPublisher(address string) *KafkaPublisher {
 		log.Fatalf("Failed to create producer: %s", err)
 	}
 
-	return &KafkaPublisher{AdminClient: adminClient, Client: producer, Topics: make(map[string]bool)}
+	return &KafkaPublisher{Client: producer, TopicID: topicID}
 }
 
-func (p KafkaPublisher) SubmitMessage(topicID string, message []byte) string {
-
-	if created, exists := p.Topics[topicID]; exists && created {
-
-		delivery_chan := make(chan kafka.Event, 10000)
-		err := p.Client.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topicID, Partition: kafka.PartitionAny},
-			Value:          message},
-			delivery_chan,
-		)
-
-		if err != nil {
-			close(delivery_chan)
-			return fmt.Sprintf("Produce failed: %s\n", err)
-		}
-
-		e := <-delivery_chan
-		m := e.(*kafka.Message)
-
-		var status string
-
-		if m.TopicPartition.Error != nil {
-			status = fmt.Sprintf("Delivery failed: %v\n", m.TopicPartition.Error)
-		} else {
-			status = fmt.Sprintf("Delivered message to topic %s [%d] at offset %v\n",
-				*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
-		}
-		close(delivery_chan)
-		return status
-	} else {
-		return fmt.Sprintf("The topic %s does not exist", topicID)
-	}
-}
-
-func (p *KafkaPublisher) NewTopic(topicID string) {
+func NewKafkaTopic(adminClient *kafka.AdminClient, topicID string) error {
 
 	topicSpecification := kafka.TopicSpecification{
 		Topic:             topicID,
@@ -77,36 +77,24 @@ func (p *KafkaPublisher) NewTopic(topicID string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	results, err := p.AdminClient.CreateTopics(ctx, []kafka.TopicSpecification{topicSpecification})
+	results, err := adminClient.CreateTopics(ctx, []kafka.TopicSpecification{topicSpecification})
 	if err != nil {
-		log.Fatalf("Failed to create topic: %s", err)
+		return fmt.Errorf("failed to create topic: %s", err)
 	}
 
 	for _, result := range results {
 		if result.Error.Code() != kafka.ErrNoError {
 			if result.Error.Code() == kafka.ErrTopicAlreadyExists {
-				log.Println("Topic Already Exists..")
-				p.Topics[topicID] = true
+				log.Printf("topic `%s` already exists", topicID)
+				return nil
 			} else {
-				log.Fatalf("Failed to create topic %s: %v", result.Topic, result.Error)
+				return fmt.Errorf("failed to create topic %s: %v", result.Topic, result.Error)
 			}
 		} else {
-			p.Topics[result.Topic] = true
 			fmt.Printf("Successfully created topic %s\n", result.Topic)
+			return nil
 		}
 	}
-}
 
-func (p KafkaPublisher) Ack() {
-	for e := range p.Client.Events() {
-		switch ev := e.(type) {
-		case *kafka.Message:
-			if ev.TopicPartition.Error != nil {
-				fmt.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
-			} else {
-				fmt.Printf("Successfully produced record to topic %s partition [%d] @ offset %v\n",
-					*ev.TopicPartition.Topic, ev.TopicPartition.Partition, ev.TopicPartition.Offset)
-			}
-		}
-	}
+	return fmt.Errorf("failed to create Topic! Did not get back any results")
 }
